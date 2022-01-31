4a0a6b3c72daa64080959f88fe9c658134b9f045
Peter-OC <polmosca@LES011428.corp.capgemini.com>
2022-01-31 12:50:06 +0100

    Decodificar Cadena

---
+++ backend/Ejercicios1/src/Capgemini/DecodificarCadena.java
@@ -0,0 +1,17 @@
+package Capgemini;
+
+public class DecodificarCadena {
+	
+	public static void main(String[] args) {
+
+		String cadena = "3+4+3,4-7*1=";
+		String[] componentes = cadena.split("(?<=[-+*=])");
+
+
+		for (int i = 0; i < componentes.length; i++) {
+		System.out.println(componentes[i]);
+		}
+
+	}
+
+}
