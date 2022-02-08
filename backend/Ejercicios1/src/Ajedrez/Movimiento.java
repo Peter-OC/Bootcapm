fca0e0b7717fcd77f8f896586a2b30ccaa8ad027
Peter-OC <polmosca@host.docker.internal>
2022-02-08 17:28:01 +0100

    Ajedrez

--- backend/Ejercicios1/src/Ajedrez/Movimiento.java
+++ backend/Ejercicios1/src/Ajedrez/Movimiento.java
@@ -2,5 +2,64 @@ package Ajedrez;
 
 public class Movimiento {
 	
+	Posicion posInicial;
+	Posicion posFinal;
+	
+	public Movimiento(Posicion posInicial, Posicion posFinal) {
+
+		this.posInicial = posInicial;
+		this.posFinal = posFinal;
+	}
+
+	public Posicion getPosInicial() {
+		return posInicial;
+	}
+
+	public void setPosInicial(Posicion posInicial) {
+		this.posInicial = posInicial;
+	}
+
+	public Posicion getPosFinal() {
+		return posFinal;
+	}
+
+	public void setPosFinal(Posicion posFinal) {
+		this.posFinal = posFinal;
+	}
+	
+	public static boolean EsVertical() {
+		return false;
+		
+	}
+	
+	public static boolean EsHorizontal() {
+		return false;
+		
+	}
+	
+	public static boolean EsDiagonal() {
+		return false;
+		
+	}
+	
+	public static int SaltoVertical() {
+		return (Integer) null;
+		
+	}
+	
+	public static int SaltoHorizontal() {
+		return (Integer) null;
+		
+	}
+	
+	public static int DeltaFila() {
+		return (Integer) null;
+		
+	}
+	
+	public static int DeltaColumna() {
+		return (Integer) null;
+		
+	}
 	
 }
