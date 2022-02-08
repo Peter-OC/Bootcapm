5b4d8930fe8baf2ab5f262b5e40a31c5cd7b57d9
Peter-OC <polmosca@host.docker.internal>
2022-02-08 17:33:32 +0100

    Ajedrez

--- backend/Ejercicios1/src/Ajedrez/JuegoException.java
+++ backend/Ejercicios1/src/Ajedrez/JuegoException.java
@@ -1,5 +1,30 @@
 package Ajedrez;
 
-public class JuegoException {
+public class JuegoException extends Exception{
+
+	public JuegoException() {
+		super();
+		// TODO Auto-generated constructor stub
+	}
+
+	public JuegoException(String message, Throwable cause, boolean enableSuppression, boolean writableStackTrace) {
+		super(message, cause, enableSuppression, writableStackTrace);
+		// TODO Auto-generated constructor stub
+	}
+
+	public JuegoException(String message, Throwable cause) {
+		super(message, cause);
+		// TODO Auto-generated constructor stub
+	}
+
+	public JuegoException(String message) {
+		super(message);
+		// TODO Auto-generated constructor stub
+	}
+
+	public JuegoException(Throwable cause) {
+		super(cause);
+		// TODO Auto-generated constructor stub
+	}
 
 }
