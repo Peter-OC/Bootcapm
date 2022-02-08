72a7c206292d13b351fb1de195e47022fbcab912
Peter-OC <polmosca@host.docker.internal>
2022-02-08 17:11:14 +0100

    Ajedrez

--- backend/Ejercicios1/src/Ajedrez/Color.java
+++ backend/Ejercicios1/src/Ajedrez/Color.java
@@ -1,30 +1,30 @@
 package Ajedrez;
 
 public enum Color {
-	BLANCAS(1), NEGRAS(2);
+	BLANCAS, NEGRAS;
 	
-	int colorValue;
-	void Colores(int value) {
-		colorValue = value;
-	}
-	
-	public int getValue() {
-		return colorValue;
-	}
-	
-	public static Color getEnum(int value) {
-		
-		switch (value) {
-		case 1: return BLANCAS;
-		case 2: return NEGRAS;
-		default: throw new IllegalArgumentException("Ese color no esta disponible");
-		}			
-	}
-
-	private int color;
-	
-	Color(int i) {
-		this.color = i;
-	}
+//	int colorValue;
+//	void Colores(int value) {
+//		colorValue = value;
+//	}
+//	
+//	public int getValue() {
+//		return colorValue;
+//	}
+//	
+//	public static Color getEnum(int value) {
+//		
+//		switch (value) {
+//		case 1: return BLANCAS;
+//		case 2: return NEGRAS;
+//		default: throw new IllegalArgumentException("Ese color no esta disponible");
+//		}			
+//	}
+//
+//	private int color;
+//	
+//	Color(int i) {
+//		this.color = i;
+//	}
 }
 
--- backend/Ejercicios1/src/Ajedrez/Posicion.java
+++ backend/Ejercicios1/src/Ajedrez/Posicion.java
@@ -1,5 +1,41 @@
 package Ajedrez;
 
 public class Posicion {
+	
+	private int laFila;
+	private int laColumna;
+	
+	public Posicion(int laFila, int laColumna) {
+		this.laFila = laFila;
+		this.laColumna = laColumna;
+	}
+	
+	public Posicion(char laFila, char laColumna) {
+		this.laFila = laFila;
+		this.laColumna = laColumna;
+	}
+
+	public int getLaFila() {
+		return laFila;
+	}
+
+//	public void setLaFila(int laFila) {
+//		this.laFila = laFila;
+//	}
+
+	public int getLaColumna() {
+		return laColumna;
+	}
+
+//	public void setLaColumna(int laColumna) {
+//		this.laColumna = laColumna;
+//	}
+	
+	@Override
+	public boolean equals(Object posicion) {
+		return super.equals(posicion);
+	}
+	
+	
 
 }
