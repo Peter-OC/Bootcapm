cffd4430c187db02187d15f42299ba381733bf38
Peter-OC <polmosca@host.docker.internal>
2022-02-10 15:22:44 +0100

    Ejercicios Iniciales

---
+++ backend/Bootcamp/src/EjerciciosIniciales/Ejercicio15.java
@@ -0,0 +1,23 @@
+//Programa que tome como dato de entrada un número que corresponde a la longitud del radio de 
+//una esfera y nos calcula y escribe el volumen de la esfera que se corresponden con dicho radio.
+//
+//La fórmula para calcular el volumen de la esfera es
+//v = (4/3)*PI*r^3
+
+package EjerciciosIniciales;
+
+import java.util.Scanner;
+
+public class Ejercicio15 {
+
+	public static void main(String[] args) {
+
+		Scanner teclado = new Scanner(System.in);
+		double radio, volumen;
+		
+		System.out.println("Introduce el rádio de la circunferencia:");		
+		radio = teclado.nextDouble();
+
+	}
+
+}
