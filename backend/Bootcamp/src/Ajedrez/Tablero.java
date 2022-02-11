885049f72fb6835ae1fbdd289bbde88f72b84346
Peter-OC <polmosca@host.docker.internal>
2022-02-11 08:16:38 +0100

    tablero

--- backend/Bootcamp/src/Ajedrez/Tablero.java
+++ backend/Bootcamp/src/Ajedrez/Tablero.java
@@ -1,7 +1,12 @@
 package Ajedrez;
 
+import java.util.ArrayList;
+import java.util.List;
+
 public class Tablero implements Cloneable{ 
 	
-	
+	private static final String Tablero = null;	
+	private List<Pieza> pieza = new ArrayList<Pieza>();
+	private Color color;	
 
 }
