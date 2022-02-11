bc7670018ea7006936ca3c1814525c2808e552f3
Peter-OC <polmosca@host.docker.internal>
2022-02-11 08:05:46 +0100

    Delta implementado

--- backend/Bootcamp/src/Ajedrez/Movimiento.java
+++ backend/Bootcamp/src/Ajedrez/Movimiento.java
@@ -1,14 +1,13 @@
 package Ajedrez;
 
-public class Movimiento { 
-	
-	Posicion posInicial;
-	Posicion posFinal;
-	
-	
-	public Movimiento(Posicion posInicial, Posicion posFinal) throws JuegoException{
-		
-		if(posInicial==null) {
+public class Movimiento {
+
+	private Posicion posInicial;
+	private Posicion posFinal;
+
+	public Movimiento(Posicion posInicial, Posicion posFinal) throws JuegoException {
+
+		if (posInicial == null) {
 			throw new JuegoException("La pieza no se ha encontrado");
 		}
 		if (posInicial.equals(posFinal)) {
@@ -36,40 +35,54 @@ public class Movimiento {
 //	}
 //	
 	public boolean EsVertical() {
-		
-		return posInicial.getLaColumna() == posFinal.getLaColumna();	
+
+		return posInicial.getLaColumna() == posFinal.getLaColumna();
 	}
-	
+
 	public boolean EsHorizontal() {
-		
-		return posInicial.getLaFila() == posFinal.getLaFila();		
+
+		return posInicial.getLaFila() == posFinal.getLaFila();
 	}
-	
+
 	public boolean EsDiagonal() {
-		
-		return SaltoVertical(0) == SaltoHorizontal(0); 
+
+		return SaltoVertical(0) == SaltoHorizontal(0);
 
 //		return posInicial.getLaColumna() != posFinal.getLaColumna() && posInicial.getLaFila() != posFinal.getLaFila();			
 	}
-	
+
 	public int SaltoVertical(int salto) {
-		
-		return (salto < posInicial.getLaFila()) ? -salto : salto;		
+
+		return (salto < posInicial.getLaFila()) ? -salto : salto;
 	}
-	
+
 	public int SaltoHorizontal(int salto) {
-		
-		return (salto < posInicial.getLaColumna()) ? -salto : salto;		
+
+		return (salto < posInicial.getLaColumna()) ? -salto : salto;
 	}
-	
-	public static int DeltaFila() {
-		return (Integer) null;
+
+	public int deltaFila() {
+		if (posFinal.getLaColumna() > posInicial.getLaColumna()) { 
+			return 1;
+		} else if (posFinal.getLaColumna() == posInicial.getLaColumna()) { 
+			return 0;
+		} else if (posFinal.getLaColumna() < posInicial.getLaColumna()) { 
+			return -1;
+		}
+		return 0;	 	
 		
 	}
-	
-	public static int DeltaColumna() {
-		return (Integer) null;
+
+	public int deltaColumna() {
+		if (posFinal.getLaFila() > posInicial.getLaFila()) {
+			return 1;
+		} else if (posFinal.getLaFila() == posInicial.getLaFila()) {
+			return 0;
+		} else if (posFinal.getLaFila() < posInicial.getLaFila()) {
+			return -1;
+		}
+		return 0;
 		
 	}
-	
+
 }
