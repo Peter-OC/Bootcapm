b745190269170d4296ac4f574c09a9a2c6773eb1
Peter-OC <polmosca@LES011428.corp.capgemini.com>
2022-02-13 21:24:59 +0100

    Ajedrez

--- backend/Bootcamp/src/Ajedrez/mainAjedrez.java
+++ backend/Bootcamp/src/Ajedrez/mainAjedrez.java
@@ -1,10 +1,5 @@
 package Ajedrez;
 
-/**
- * @author: Mario R. Rancel
- * @see <a href = "https://github.com/Peter-OC/Bootcapm/tree/main/backend/Bootcamp/src/Ajedrez" /> Repositorio con todos los archivos </a>
- */
-
 public class mainAjedrez { 
 
 	public static void main(String[] args) {
