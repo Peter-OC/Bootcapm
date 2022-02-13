0f3eed874444a8cf55a3aa2e97d806a62249b96e
Peter-OC <polmosca@LES011428.corp.capgemini.com>
2022-02-13 21:36:16 +0100

    Añadido documentación JavaDocs

--- backend/Bootcamp/src/Ajedrez/Posicion.java
+++ backend/Bootcamp/src/Ajedrez/Posicion.java
@@ -2,10 +2,22 @@ package Ajedrez;
 
 import java.util.Objects; 
 
+
+/**
+ * @author: Pedro Olmos
+ * @see <a href = "https://github.com/Peter-OC/Bootcapm/tree/main/backend/Bootcamp/src/Ajedrez" /> Repositorio con todos los archivos </a>
+ */
+
 public class Posicion {
 	
 	private int laFila;
 	private int laColumna;
+	
+	
+	/**
+	 * @param: Métodos que validan si un movimiento es valido en la fila y en la columna
+	 * @throws: Devuelve una excepción de la clase JuegoException
+	 */
 	
 	public Posicion(int laFila, int laColumna) throws JuegoException {
 		
@@ -26,6 +38,11 @@ public class Posicion {
 			}
 		
 	}
+	
+	/**
+	 * 
+	 * @return: getLaFila y getLaColumna devuelven la fila y la columna, indispensable para las clases de movimiento y de pieza
+	 */
 
 	public int getLaFila() {
 		return laFila;
@@ -42,7 +59,7 @@ public class Posicion {
 //	public void setLaColumna(int laColumna) {
 //	this.laColumna = laColumna;
 //}
-
+	
 	@Override
 	public int hashCode() {
 		return Objects.hash(laColumna, laFila);
